dd8cfea52d2e864e7fb9299f28e39aa5a870734c
Prince thakur <thakurprince07869@gmail.com>
2024-04-11 18:03:13 +0530

    project done

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../Components/Header/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
+import AppDownlode from '../../Components/AppDownlode/AppDownlode'
+
+const Home = () => {
+
+    const [category, setCategory] = useState("All")
+
+    return (
+        <div>
+        <Header/>
+        <ExploreMenu category={category} setCategory={setCategory}/>
+        <FoodDisplay category={category}/>
+        <AppDownlode/>
+        </div>
+    )
+}
+
+export default Home
